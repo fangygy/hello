844a93e535f047b10182007c2a9e4d9f40a842fe
Yuan Fu <fuyuanx@gmail.com>
2017-06-26 21:21:56 +0800

    drone follow & security follow(crappy animation

---
+++ Assets/Scripts/droneDectection.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class droneDectection : MonoBehaviour {
+	public float droneMovementSpeed = 0.03f;
+	public float yOffset = 2f;
+	public float xOffset = 2f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.gameObject.tag == "ControlledPlayer") {
+			//call drone over
+			GameObject drone = this.transform.parent.gameObject;
+			Vector3 playersPos = new Vector3 (other.gameObject.transform.position.x, other.gameObject.transform.position.y, other.gameObject.transform.position.z);
+			playersPos.y += yOffset;
+			playersPos.x += xOffset;
+			drone.transform.position = Vector3.MoveTowards (drone.transform.position, playersPos, droneMovementSpeed);
+
+			//drone now in place, maybe freeze the player here for a security scan;
+
+
+
+			//found invader
+			commWithSecurity(other.gameObject);
+
+		}
+	}
+
+	void commWithSecurity(GameObject invader){
+		GameObject security = GameObject.Find ("security");
+		securityMovement script = security.GetComponent<securityMovement>();
+		script.needSecurityHelp (invader);
+		//GameObject security2 = GameObject.Find ("security2");
+	}
+}
---
+++ Assets/Scripts/securityMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class securityMovement : MonoBehaviour {
+	public float securityMovementSpeed = 0.5f;
+	public float securityRotationSpeed = 1f;
+	public float attackRange = 2.0f;
+	private bool isInvaded = false;
+	private GameObject targetToClear;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("security awaken");
+
+		if (isInvaded) {
+			float Distance = Vector3.Distance (transform.position, targetToClear.transform.position);
+			Debug.LogWarning (Distance);
+			if(Distance > attackRange){
+				//chase the target
+				transform.position = Vector3.MoveTowards(transform.position, targetToClear.transform.position, securityMovementSpeed);
+				//look at the target
+				Quaternion targetRotation = Quaternion.LookRotation(targetToClear.transform.position - transform.position);
+				Vector3 cross = Vector3.Cross(transform.rotation*Vector3.forward, targetRotation*Vector3.forward);
+
+				transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, securityRotationSpeed);
+
+				if (cross.y < 0) {
+					//right turn
+					updateAniation (1.0f, -0.7f);
+				} else {
+					//left turn
+					updateAniation (1.0f, 0.7f);
+				}
+			}
+			else{
+				//attack
+
+				updateAniation (0f, 0f);
+			}
+		}
+	}
+
+	void updateAniation(float forward_amt, float turn_amt){
+		
+		anim.SetFloat("Forward", forward_amt, 0.1f, Time.deltaTime);
+		anim.SetFloat("Turn", turn_amt, 0.1f, Time.deltaTime);
+
+	}
+
+	public void needSecurityHelp(GameObject invader){
+
+		isInvaded = true;
+		targetToClear = invader;
+	}
+}
